531cb2bdfb93eaff670e2ae81354dea39956bbfd
nipagames <54GiHuSaSaOnHy65>
2022-03-19 22:04:25 +0200

    fixed test build

--- build-test.bat
+++ build-test.bat
@@ -1,2 +1,2 @@
 @echo off
-g++ sfxr-test.cpp -I include SFXR.dll -o bin/SFXR_test.exe -static-libgcc -static-libstdc++ -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic || echo Compilation failed!
+g++ sfxr-test.cpp -I include bin/SFXR.dll -o bin/SFXR_test.exe -static-libgcc -static-libstdc++ -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic || echo Compilation failed!
